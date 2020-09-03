2c76b6693bd1e9c574539435c43e09911f61280a
Viktor <vitosbat@gmail.com>
2020-09-03 22:39:36 +0700

    Init commit

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour
+{
+    public Sprite musicOn, musicOff;
+
+    public void Start()
+    {
+        if (PlayerPrefs.GetString("music") == "No" && gameObject.name == "Music")
+        {
+            PlayerPrefs.SetString("music", "Yes");
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+    public void RestartGame()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+   
+    public void MusicWork()
+    {
+        if (PlayerPrefs.GetString("music") == "No")
+        {
+            GetComponent<AudioSource>().Play();
+            PlayerPrefs.SetString("music", "Yes");
+            GetComponent<Image>().sprite = musicOn;
+        } else
+        {
+            PlayerPrefs.SetString("music", "No");
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+}
